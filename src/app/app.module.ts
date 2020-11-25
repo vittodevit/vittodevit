820a64a95f9a8bb4b7cfe50638ad9199567df683
Vittorio Lo Mele <vittorio@mrbackslash.it>
2020-11-25 12:48:19 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { MieiprogettiComponent } from './mieiprogetti/mieiprogetti.component';
+import { FooterComponent } from './footer/footer.component';
+import { SudimeOldComponent } from './sudime-old/sudime-old.component';
+import { FeaturettePresentazioneComponent } from './featurette-presentazione/featurette-presentazione.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    MieiprogettiComponent,
+    FooterComponent,
+    SudimeOldComponent,
+    FeaturettePresentazioneComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sudime-old/sudime-old.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sudime-old',
+  templateUrl: './sudime-old.component.html',
+  styles: [
+  ]
+})
+export class SudimeOldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
