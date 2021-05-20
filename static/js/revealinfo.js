45e6a3882ce21ac51d01be26aa6727fe69948359
Vittorio Lo Mele <vittorio@mrbackslash.it>
2021-05-20 02:00:00 +0200

    new email address

--- static/js/revealinfo.js
+++ static/js/revealinfo.js
@@ -1,32 +1,14 @@
 //this is to avoid spambot
-var efh38f834y5 = 
-["dml0dG9yaW9AbX",
-"JiYWNrc2xhc2guaXQ",
-"FzaF9pdCxodHRw",
-"mFja3NsYXNoLml0",
-"dHRvcmlvQG1yY",
-"WNrc2xhc2hfaXQ=",
-"LEBtcmJhY2tzbG",
-"sbWFpbHRvOnZp",
-"czovL3QubWUvbXJiY",];
-
-var r9454jt5ijg5 = [
-    efh38f834y5[0],
-    efh38f834y5[1],
-    efh38f834y5[7],
-    efh38f834y5[4],
-    efh38f834y5[3],
-    efh38f834y5[6],
-    efh38f834y5[2],
-    efh38f834y5[8],
-    efh38f834y5[5]
+var info = [
+ "mailto:hi@vitto.dev",
+ "hi@vitto.dev",
+ "https://t.me/mrbackslash_it",
+ "@mrbackslash_it"
 ];
-
-var grit0459utj54 = atob(r9454jt5ijg5.join().replaceAll(',', '')).split(',');
-document.getElementById("campomail").href = grit0459utj54[1];
-document.getElementById("campomail").innerText = grit0459utj54[0];
-document.getElementById("campotelegram").href = grit0459utj54[3];
-document.getElementById("campotelegram").innerText = grit0459utj54[2];
+document.getElementById("campomail").href = info[0];
+document.getElementById("campomail").innerText = info[1];
+document.getElementById("campotelegram").href = info[2];
+document.getElementById("campotelegram").innerText = info[3];
 
 var typed = new Typed('#testoscritto', {
     strings: ["un ragazzo di 15 anni.", "uno sviluppatore.", "un appassionato di informatica.", "un system administrator.", "assetato di conoscenza."],
