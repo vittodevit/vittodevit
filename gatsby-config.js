9fb34558dbabd55612c6fa743e7e9670fd11f89f
Vittorio Lo Mele <vittorio@mrbackslash.it>
2022-03-04 16:51:09 +0100

    metrics move output + gatsby 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
       title: `Vittorio Lo Mele`,
     siteUrl: `https://vitto.dev`
   },
-  plugins: []
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
